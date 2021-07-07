3661b56f23ac657bcbd7891f9ae42717e8129602
Fatih Sultan Ay <f.sultanay@gmail.com>
2021-07-06 20:04:44 -0400

    deleted job post
    deleted Accounting post

--- src/Pages/Career/index.js
+++ src/Pages/Career/index.js
@@ -31,8 +31,8 @@ function Career() {
                             link="https://ca.indeed.com/Goldie-Mohr-jobs-in-Ontario" />
                         <Job title="AZ Truck Driver" description="We are looking to hire a reliable and hard-working AZ Driver to transport goods to customers in a timely manner. The AZ Driver's responsibilities include safely securing goods to prevent damage, identifying ways to minimize fuel consumption where possible, as well as supervising the safe loading and unloading of goods onto and off the company truck. You should also be able to maintain a record of all maintenance activities and repairs performed on the company truck."
                             link="https://ca.indeed.com/Goldie-Mohr-jobs-in-Ontario" />
-                        <Job title="Administration/Accounting" description="We are looking for an Accounts Administrator to manage our company’s accounts payable and receivable.Accounts Administrator responsibilities include reviewing and reconciling accounts, processing payments to external partners and maintaining updated records of invoices and receipts. To be successful in this role, you should have a Finance or Accounting background and be familiar with bookkeeping."
-                            link="https://ca.indeed.com/Goldie-Mohr-jobs-in-Ontario" />
+                        {/* <Job title="Administration/Accounting" description="We are looking for an Accounts Administrator to manage our company’s accounts payable and receivable.Accounts Administrator responsibilities include reviewing and reconciling accounts, processing payments to external partners and maintaining updated records of invoices and receipts. To be successful in this role, you should have a Finance or Accounting background and be familiar with bookkeeping."
+                            link="https://ca.indeed.com/Goldie-Mohr-jobs-in-Ontario" /> */}
 
                     </div>
                 </div>
